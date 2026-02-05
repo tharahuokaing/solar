dbdce8d68a6da2579a5abc49db0f868b7325638d
THARA HUOKAING <tharahuokaing@gmail.com>
2026-02-05 16:53:36 +0700

    Change AI voice language from English to Khmer

--- script2.js
+++ script2.js
@@ -10,7 +10,7 @@ const logFeed = document.getElementById('log-feed');
 // 1. AI Voice System
 function aiSpeak(msg) {
     const speech = new SpeechSynthesisUtterance(msg);
-    speech.lang = 'en-US';
+speech.lang = 'km-KH';
     speech.rate = 1.0;
     speech.pitch = 0.6;
     window.speechSynthesis.speak(speech);
